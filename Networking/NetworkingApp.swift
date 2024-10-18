3c99ffb27518b6334a9d61d3b5a0160eb56590bc
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2024-10-18 16:41:35 +0200

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by Gaetan Salvi on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
